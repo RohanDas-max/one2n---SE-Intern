1570bf6fb443a017672ab09763c2f8d57a109ac3
rohandas-max <rohandasd0@gmail.com>
2022-02-04 20:15:31 +0530

    only josephus

---
+++ week1/josephusProblem/flags.go
@@ -0,0 +1,11 @@
+package main
+
+import "flag"
+
+func flags() (int, int) {
+
+	n := flag.Int("n", 14, "option to get the numbers players")
+	k := flag.Int("k", 2, "option to get the skips")
+	flag.Parse()
+	return *n, *k
+}
--- week1/josephusProblem/main.go
+++ week1/josephusProblem/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func exec() {
+	n, k := flags()
+	res := Controller(n, k)
+	fmt.Printf("%d number player is the winner\n", res)
+}
+
 func main() {
-	fmt.Println("hello joseph")
+	exec()
 }
