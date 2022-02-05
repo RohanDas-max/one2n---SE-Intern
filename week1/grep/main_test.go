ff60a56b055b434c208b7b0a8002b4d66f7b6efe
rohandas-max <rohandasd0@gmail.com>
2022-02-05 22:24:36 +0530

    test added

--- week1/grep/main_test.go
+++ week1/grep/main_test.go
@@ -6,8 +6,12 @@ func Test_main(t *testing.T) {
 	tests := []struct {
 		name string
 	}{
-		// TODO: Add test cases.
+
+		{
+			name: "1",
+		},
 	}
+	wg.Add(len(tests))
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			main()
