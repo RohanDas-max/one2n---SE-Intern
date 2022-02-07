773cbdd3e7a9bc209beb0c08a3e9e99b4fe74fa1
rohandas-max <rohandasd0@gmail.com>
2022-02-07 10:05:27 +0530

    test added

--- week1/grep/searchFromStdin_test.go
+++ week1/grep/searchFromStdin_test.go
@@ -1,13 +1,30 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestSearchstdin(t *testing.T) {
+	type args struct {
+		arg string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "foo",
+			args: args{
+				arg: "foo",
+			},
+			wantErr: false,
+		},
+	}
 	wg.Add(1)
-	err := Searchstdin("hello")
-	if err != nil {
-		t.Errorf("Searchstdin() error = %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Searchstdin(tt.args.arg); (err != nil) != tt.wantErr {
+				t.Errorf("Searchstdin() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
